184288f37cfe14b3dcc6ced4dcc3aa8255ca3ec1
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-29 12:07:56 -0600

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Juan Carlos Robledo Morales on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashzillaTests/FlashzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaTests.swift
+//  FlashzillaTests
+//
+//  Created by Juan Carlos Robledo Morales on 29/11/24.
+//
+
+import Testing
+@testable import Flashzilla
+
+struct FlashzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
