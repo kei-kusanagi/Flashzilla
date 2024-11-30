6b8cd0f8a6d5739a9acf45767e75f5e565d2fa85
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-29 19:10:59 -0600

    Disabling user interactivity with allowsHitTesting()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,92 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var currentAmount = Angle.zero
-//    @State private var finalAmount = Angle.zero
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
     
     var body: some View {
-
-                let dragGesture = DragGesture()
-                    .onChanged { value in offset = value.translation }
-                    .onEnded { _ in
-                        withAnimation {
-                            offset = .zero
-                            isDragging = false
-                        }
-                    }
-
-            
-                let pressGesture = LongPressGesture()
-                    .onEnded { value in
-                        withAnimation {
-                            isDragging = true
-                        }
-                    }
-
-            
-                let combined = pressGesture.sequenced(before: dragGesture)
-
-                Circle()
-                    .fill(.red)
-                    .frame(width: 64, height: 64)
-                    .scaleEffect(isDragging ? 1.5 : 1)
-                    .offset(offset)
-                    .gesture(combined)
-            }
-        
-        
-//        VStack{
-//            Text("Hello, world!")
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped!")
+        }
+//        ZStack {
+//            Rectangle()
+//                .fill(.blue)
+//                .frame(width: 300, height: 300)
 //                .onTapGesture {
-//                    print("Text tapped")
+//                    print("Rectangle tapped!")
 //                }
+//            
+//            Circle()
+//                .fill(.red)
+//                .frame(width: 300, height: 300)
+//                .contentShape(.rect)
+//                .onTapGesture {
+//                    print("Circle tapped!")
+//                }
+//                .allowsHitTesting(false)
 //        }
-////        .highPriorityGesture(
-//        .simultaneousGesture(
-//        TapGesture()
-//            .onEnded {
-//            print("Vstack tapped")
-//        }
-//        )
-        
-//            .scaleEffect(finalAmount + currentAmount)
-//            .rotationEffect(currentAmount + finalAmount)
-//            .gesture(
-//                RotateGesture()
-//                    .onChanged { value in
-//                        currentAmount = value.rotation
-//                        
-//                    }
-//                    .onEnded { value in
-//                        finalAmount += currentAmount
-//                        currentAmount = .zero
-//                    }
-//            )
-//            .gesture(
-//                MagnifyGesture()
-//                    .onChanged{ value in
-//                        currentAmount = value.magnification - 1
-//                    }
-//                    .onEnded{ value in
-//                        finalAmount += currentAmount
-//                        currentAmount = 0
-//                    }
-//                )
-//            .onTapGesture (count: 2){
-//                print("Double trapped")
-//            }
-        
-        
-//            .onLongPressGesture(minimumDuration: 0){
-//                print("Long pressed")
-//            } onPressingChanged: { inProgress in
-//                print("In progress: \(inProgress)")
-//            }
-        
-       
-    
+    }
 }
 
 #Preview {
